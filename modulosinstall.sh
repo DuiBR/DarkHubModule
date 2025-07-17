cee467a0d2e17a08fc98b3bba6fc42bab2203c73
DuiBR <95405352+DuiBR@users.noreply.github.com>
2025-07-17 10:07:17 -0300

    Add files via upload

--- modulosinstall.sh
+++ modulosinstall.sh
@@ -1,42 +1,28 @@
 #!/bin/bash
 export PATH=$PATH:/usr/sbin
+
+# Desativa IPv6 temporariamente
 sysctl -w net.ipv6.conf.all.disable_ipv6=1
 sysctl -w net.ipv6.conf.default.disable_ipv6=1
 
+# Detecta arquitetura e baixa o jq correspondente
 cake=$(uname -m)
 wget "https://raw.githubusercontent.com/DuiBR/DarkHubModule/main/${cake}" -O jq
 chmod +x jq
 
-if [ -d "/opt/DragonCore/" ]; then
-    rm -f atlas*.sh delete.py sincronizar.py add*.sh rem*.sh
-    wget "https://raw.githubusercontent.com/DuiBR/DarkHubModule/main/dragon.sh"   -O dragonmodule
-    wget "https://raw.githubusercontent.com/DuiBR/DarkHubModule/main/delete.py"     -O delete.py
-    wget "https://raw.githubusercontent.com/DuiBR/DarkHubModule/main/sincronizar.py" -O sincronizar.py
-    chmod 777 dragonmodule delete.py sincronizar.py
+# Remove scripts antigos
+rm -f atlasdata.sh atlascreate.sh atlasteste.sh atlasremove.sh delete.py sincronizar.py add.sh rem.sh addteste.sh addsinc.sh remsinc.sh
 
-    if [ -d "/opt/sshplus/" ]; then
-        rm -f atlas*.sh delete.py sincronizar.py add*.sh rem*.sh
-        wget "https://raw.githubusercontent.com/DuiBR/DarkHubModule/main/sshplus.sh"   -O dragonmodule
-        wget "https://raw.githubusercontent.com/DuiBR/DarkHubModule/main/delete.py"     -O delete.py
-        wget "https://raw.githubusercontent.com/DuiBR/DarkHubModule/main/sincronizar.py" -O sincronizar.py
-        chmod 777 dragonmodule delete.py sincronizar.py
-    else
-        rm -f atlas*.sh delete.py sincronizar.py add*.sh rem*.sh
-        wget "https://raw.githubusercontent.com/DuiBR/DarkHubModule/main/sshplus.sh"   -O dragonmodule
-        wget "https://raw.githubusercontent.com/DuiBR/DarkHubModule/main/delete.py"     -O delete.py
-        wget "https://raw.githubusercontent.com/DuiBR/DarkHubModule/main/sincronizar.py" -O sincronizar.py
-        chmod 777 dragonmodule delete.py sincronizar.py
-    fi
+# Baixa os módulos atualizados
+wget "https://raw.githubusercontent.com/DuiBR/DarkHubModule/main/sshplus.sh" -O dragonmodule
+wget "https://raw.githubusercontent.com/DuiBR/DarkHubModule/main/delete.py" -O delete.py
+wget "https://raw.githubusercontent.com/DuiBR/DarkHubModule/main/sincronizar.py" -O sincronizar.py
+chmod 777 dragonmodule delete.py sincronizar.py
 
-else
-    # Caso você queira fazer algo diferente quando /opt/DragonCore não existir,
-    # coloque aqui. Se não, pode remover este else.
-    :
-fi
-
-# Continua o resto do script
-wget "https://raw.githubusercontent.com/DuiBR/DarkHubModule/main/verificador.py" -O verificador.py
+# Executa verificador
+wget "https://raw.githubusercontent.com/DuiBR/DarkHubModule/main/verificador.py" -O verificador.py 
 python3 verificador.py
 
+# Reativa IPv6
 sysctl -w net.ipv6.conf.all.disable_ipv6=0
 sysctl -w net.ipv6.conf.default.disable_ipv6=0
